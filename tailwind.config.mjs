5e9d71cf069f78a4cc2a2288db703d2279559644
hayden Tomas <hello@haydentomas.com>
2024-06-13 14:11:00 +0100

    following shared layout tutorial at JQ to build shared layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		  },
+		extend: {
+			fontFamily: {
+				sans: ['Cabin', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/typography'),
-		
+
 	],
 }
